c5dab4225f46567804297c506cbaf64bbb19543c
Florian Porsch <Tederean@users.noreply.github.com>
2022-05-09 09:01:16 +0200

    Add files via upload

---
+++ Maui_SfCartesianChart_Autosize/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui_SfCartesianChart_Autosize;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Maui_SfCartesianChart_Autosize/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui_SfCartesianChart_Autosize;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
